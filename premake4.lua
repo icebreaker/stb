9cab4e922819e435c4c391e0b91870235456d275
icebreaker <theicebreaker007@gmail.com>
2012-01-07 22:28:30 +0200

    added install and prefix options

--- premake4.lua
+++ premake4.lua
@@ -12,6 +12,33 @@ solution "stb"
 	  kind "StaticLib"
   	files { "stb_vorbis.c" }
 
-   project "stb_truetype"
+  project "stb_truetype"
 	  kind "StaticLib"
-  	files { "stb_truetype.c" }
+    files { "stb_truetype.c" }
+
+newoption
+{
+  trigger = "prefix",
+  description = "Install prefix"
+}
+
+newaction 
+{
+	trigger     = "install",
+	description = "Install into the location specified by --prefix.",
+	execute		  = function()
+    if _OPTIONS["prefix"] then
+      local libdir = path.join(_OPTIONS["prefix"], "lib")
+      local incdir = path.join(_OPTIONS["prefix"], "include")
+      
+      os.copyfile("stb_image.h"   , incdir)
+      os.copyfile("stb_vorbis.h"  , incdir)
+      os.copyfile("stb_truetype.h", incdir)
+      os.copyfile("build/libstb_image.a"    , libdir)
+      os.copyfile("build/libstb_vorbis.a"   , libdir)
+      os.copyfile("build/libstb_truetype.a" , libdir)
+    else
+      print("No --prefix was specified.")
+    end
+	end
+}
